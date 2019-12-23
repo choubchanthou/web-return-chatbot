d2be591c248dee6da0f83daf68811169a44c63b0
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2019-12-23 15:19:48 +0700

    Add messenger Token

--- server.js
+++ server.js
@@ -13,16 +13,10 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Proxy version 1.0'));
 app.get('/webhook', function (req, res) {
-    // var messaging_events = req.body.entry[0].messaging;
-    // for (var i = 0; i < messaging_events.length; i++) {
-    //     var event = req.body.entry[0].messaging[i];
-    //     var sender = event.sender.id;
-    //     if (event.message && event.message.text) {
-    //         var text = event.message.text;
-    //         sendTextMessage(sender, text + "!");
-    //     }
-    // }
-    res.sendStatus(200);
+    if (req.query['hub.verify_token'] === process.env.FB_TOKEN) {
+		res.send(req.query['hub.challenge']);
+	}
+	res.send('Wrong token!');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
