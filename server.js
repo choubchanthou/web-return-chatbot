eebf2d93a69b3d8072c286a20883c677f073937e
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2019-12-23 15:09:41 +0700

    Add script start

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const request = require('request');
 const app = express();
 
 app.set('port', (process.env.PORT || 8000));
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
