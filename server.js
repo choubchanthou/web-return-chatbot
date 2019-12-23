791edb6c9f2cadd31ac9154f5b40665661a3cfc6
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2019-12-23 15:14:23 +0700

    app

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
-app.set('port', (process.env.PORT || 8000));
+// app.set('port', (process.env.PORT || 8000));
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,15 +13,15 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Proxy version 1.0'));
 app.get('/webhook', function (req, res) {
-    var messaging_events = req.body.entry[0].messaging;
-    for (var i = 0; i < messaging_events.length; i++) {
-        var event = req.body.entry[0].messaging[i];
-        var sender = event.sender.id;
-        if (event.message && event.message.text) {
-            var text = event.message.text;
-            sendTextMessage(sender, text + "!");
-        }
-    }
+    // var messaging_events = req.body.entry[0].messaging;
+    // for (var i = 0; i < messaging_events.length; i++) {
+    //     var event = req.body.entry[0].messaging[i];
+    //     var sender = event.sender.id;
+    //     if (event.message && event.message.text) {
+    //         var text = event.message.text;
+    //         sendTextMessage(sender, text + "!");
+    //     }
+    // }
     res.sendStatus(200);
 });
 
