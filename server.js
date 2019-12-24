ef626bf5b41272e8a02bc960bf343a8722c59bf2
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2019-12-24 18:17:42 +0700

    Add create shipback by order

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post('/webhook/', function(req, res) {
         let db = JSON.parse(rawdata);
         if (event.message && event.message.text) {
             var text = event.message.text;
-            if (db.store_name == undefined) {
+            if (db.store_name == 0) {
                 if (hasAvailable(text)){
                     db.store_name = text;
                     const json = JSON.stringify(db);
