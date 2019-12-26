805be7c5e572454ce955e77308cfb77dea131f4d
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2019-12-26 16:32:53 +0700

    test

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ app.post('/webhook/', async (req, res) => {
                     return await sendTextMessage(sender, "Please enter your order number: ");
                 }
                 // return await sendTextMessage(sender, message.length);
-                await sendTextMessage(sender, `You have an order(${order_id}) selected already!. Please say [hi] if you want to new return`);
-                event.message.text = '';
-                return await sendMessagebyOrder(sender, order_id);
+                // await sendTextMessage(sender, `You have an order(${order_id}) selected already!. Please say [hi] if you want to new return`);
+                // event.message.text = '';
+                // return await sendMessagebyOrder(sender, order_id);
             }
             if(step == undefined) {
                 const has_store_available = await hasAvailable(name);
