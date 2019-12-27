1c115f5be49067a264e1359201f56dfff8b09ea0
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2019-12-27 15:08:56 +0700

    test

--- server.js
+++ server.js
@@ -109,18 +109,18 @@ const hasStoreRef = async (sender, store, order_id) => {
     }
     return false;
 };
-const hasAllRef = async (sender, store, order_id) => {
+const hasAllRef = async (sender, store, _order_id) => {
     try {
-        if (store !== null && order_id !== null) {
+        if (store !== null && _order_id !== null) {
             const is_avail = await hasAvailable(store);
             const { store_name, order_id, step } = await fetchSessionSender(sender) || {};
             if (!is_avail) return await sendTextMessage(sender, `Sorry, your store(${store}) has not registed. Please try again!`);
             if (store_name == undefined && order_id == undefined) {
-                await insertOne("sessions", { sender, store_name: store, order_id, step: 1 });
+                await insertOne("sessions", { sender, store_name: store, order_id: _order_id, step: 1 });
             } else {
-                await saveOrderIdBySender(sender, { store_name: store, order_id, step: 1 });
+                await saveOrderIdBySender(sender, { store_name: store, order_id: _order_id, step: 1 });
             }
-            const has_selected_order = await hasSelectedOrder(sender, order_id, order_id);
+            const has_selected_order = await hasSelectedOrder(sender, _order_id, _order_id);
             if (has_selected_order) return true;
             if (step == 1) { 
                 return await sendMessagebyOrder(sender, order_id);
