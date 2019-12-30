db8958c53dbdaf6be94dbf051d26615437d6c9aa
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2019-12-30 17:35:31 +0700

    test

--- server.js
+++ server.js
@@ -66,9 +66,9 @@ app.post('/webhook/', async (req, res) => {
     res.sendStatus(200);
 });
 app.post('/shipbacks/finish', async (req, res) => {
-    const { sender, label_url } = req.body || {};
-    await update('sessions', { order_id: null, step: 1 }, { sender });
-    await sendMessageButton(sender, 'Download label', "Please download lable below:",label_url, "false");
+    // await update('sessions', { order_id: null, step: 1 }, { sender });
+    // await sendMessageButton(sender, 'Download label', "Please download lable below:",label_url, "false");
+    await sendTextMessage('2701680329926002', JSON.stringify(req.body));
     res.json({ success: true });
 });
 app.get('/orders/:id', async (req, res) => {
