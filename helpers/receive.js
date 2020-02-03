aa0c2e919d22815c98b082daf66fa42a6bbc2818
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-03 18:16:15 +0700

    Fix undfined access token

--- helpers/receive.js
+++ helpers/receive.js
@@ -2,14 +2,26 @@ const fbSend = require('./send');
 const query = require('./query');
 const elements = [
     {
-        title: "Welcome!",
+        title: "Etam",
         image_url: "https://s3.amazonaws.com/srb-staging/companies/817/e7d/55-/logos/main.png?1549875154",
-        subtitle: "We have the right hat for everyone.",
+        subtitle: "Return with etam store",
         buttons: [
             {
                 type: "postback",
-                title: "Postback Button",
-                payload: "DEVELOPER_DEFINED_PAYLOAD"
+                title: "Etam",
+                payload: "ETAM_STORE"
+            }
+        ]
+    },
+    {
+        title: "Dragon Store",
+        image_url: "https://s3.amazonaws.com/srb-staging/companies/19e/ba6/d9-/logos/main.png?1563670189",
+        subtitle: "Return with dragon store",
+        buttons: [
+            {
+                type: "postback",
+                title: "Dragon store",
+                payload: "DRAGON_STORE"
             }
         ]
     }
