2551e669a4cf099ef22bfb92898fdbefe55045ed
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-03 12:10:38 +0700

    Add test for handle receive message

--- helpers/receive.js
+++ helpers/receive.js
@@ -1,8 +1,9 @@
 import fbSend from './send';
 
 const handleReceiveMessage = (event) => {
+    const access_token = "EAAuL0vyhZBO0BAEcJ5DOkZADKzsKcYMDxK1QMdsZCs2Np2xCZAnrDPQqKGoLC5LVha07QVXxApYVc94SJLDDqy4KqV9wZAoXwjtl66SstkeoYIYsvNL3mZA31JW8rnBzn4sPKPNDDxuUAipKG9veAiT63ewVqI3YC8Ik5ljbM4UqngQ1PqfC04PKqn3lU7oIsZD";
     const message = event.message;
     const senderId = event.sender.id;
     fbSend.sendReadReceipt(senderId);
-    if (message.text) { fbSend.sendTextEnterOrderId(senderId); }
+    if (message.text) { fbSend.sendTextEnterOrderId(senderId, access_token); }
 };
--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,11 +1,28 @@
 const express = require('express');
-// import fbReceive from '../helpers/receive';
+import fbReceive from '../helpers/receive';
 const router = express.Router();
 router.get('/', (req, res) => {
     if (req.query['hub.verify_token'] === process.env.WEBHOOK_TOKEN) {
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Error, wrong token');
+    }
+});
+
+router.post('/', (req, res) => {
+    res.sendStatus(200);
+    const data = req.body;
+    if (data.object === 'page') {
+        data.entry.forEach((pageEntry) => {
+            pageEntry.messaging.forEach((messagingEvent) => {
+                console.log({ messagingEvent });
+                if (messagingEvent.message) {
+                    fbReceive.handleReceiveMessage(messagingEvent);
+                } else {
+                    console.log('Webhook received unknown messagingEvent: ', messagingEvent);
+                }
+            });
+        });
     }
 });
 module.exports = router;
