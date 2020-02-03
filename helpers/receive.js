e3ee12b6f026db78b3bfb6411ebf27067b31a100
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-03 14:12:02 +0700

    Replace import to require

--- helpers/receive.js
+++ helpers/receive.js
@@ -5,7 +5,7 @@ const handleReceiveMessage = (event) => {
     const message = event.message;
     const senderId = event.sender.id;
     fbSend.sendReadReceipt(senderId);
-    if (message.text) { fbSend.sendTextEnterOrderId(senderId, access_token); }
+    // if (message.text) { fbSend.sendTextEnterOrderId(senderId, access_token); }
 };
 
 module.exports = {
