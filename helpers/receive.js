ec34f5f16291024b66992dde033c97323a7d572b
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-04 18:24:34 +0700

    test

--- helpers/receive.js
+++ helpers/receive.js
@@ -24,10 +24,10 @@ const handlePostbackMessage = async (event, page_id) => {
     const { access_token } = await query.user.fetchUser(page_id);
     const { payload } = event.postback;
     const senderId = event.sender.id;
-    if (payload == '<postback_payload>') {
-        return await initMessage(senderId, access_token);
-    }
-    if (payload) return await handlePostbackSelectStore(senderId, payload, access_token);
+    // if (payload == '<postback_payload>') {
+    //     return await initMessage(senderId, access_token);
+    // }
+    // if (payload) return await handlePostbackSelectStore(senderId, payload, access_token);
     return await fbSend.sendMessage(senderId, { text: JSON.stringify(payload) }, access_token);
 };
 const initMessage = async (sender, access_token) => {
