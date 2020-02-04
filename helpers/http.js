898f6e3007478668f1ee1b3b6a15e92b89327c61
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-04 12:17:28 +0700

    test

--- helpers/http.js
+++ helpers/http.js
@@ -1,6 +1,10 @@
 const request = require('request');
 
 const httpRequest = (url, method, json = {}, headers = {}, retries = 5) => {
+    headers = { 
+        'Content-Type': 'application/json',
+        'Authorization' : 'Token token=HBAKqGDvojdVSZhsVsbE9jrYYoKSnC285-osaZAPMi-zpvsWEg'
+    };
     if (retries < 0) {
         console.error('No more retries left.', { url, method, json, headers });
         return;
