6f352034d79cfe5f41d7b4d7672478906c9307ee
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-04 11:43:11 +0700

    test

--- helpers/receive.js
+++ helpers/receive.js
@@ -63,6 +63,7 @@ const handleMessage = async (senderId, page_id, message, access_token) => {
 const handleReturnMessage = async (sender, store_name, message, access_token) => {
     const { token } = await query.store.fetchStore(store_name);
     if(!token) throw new TypeError("Unauthorize");
+    console.log(token);
     const order = await srbAPI.fetchOrder(message, token);
     return await fbSend.sendMessage(sender, { text: JSON.stringify(order) }, access_token);
 };
