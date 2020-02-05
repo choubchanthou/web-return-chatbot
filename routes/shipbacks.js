08ed87a82d6fb5a5182cba7cb078febbabfe8d37
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-05 10:53:51 +0700

    Add new option new and new2

--- routes/shipbacks.js
+++ routes/shipbacks.js
@@ -5,13 +5,14 @@ const query = require('../helpers/query');
 const srbAPI = require('../helpers/srb.service');
 
 router.post('/shipbacks/finish', async (req, res) => {
-    const { order_number, label_url, voucher_url, page_id } = req.body || {};
-    const { sender } = await query.session.find({ order_id: order_number });
-    const { access_token } = await query.user.find({ page_id });
-    await query.session.delete({ sender });
-    await fbSend.sendReadReceipt(sender, access_token);
-    await fbSend.sendDownloadLabelVoucher(sender,{ label_url, voucher_url }, access_token);
-    res.json({ success: true });
+    // const { order_number, label_url, voucher_url, page_id } = req.body || {};
+    // const { sender } = await query.session.find({ order_id: order_number });
+    // const { access_token } = await query.user.find({ page_id });
+    // await query.session.delete({ sender });
+    // await fbSend.sendReadReceipt(sender, access_token);
+    // await fbSend.sendDownloadLabelVoucher(sender,{ label_url, voucher_url }, access_token);
+    console.log(req.body);
+    res.json(req.body);
 });
 
 router.get('/shipbacks/test', async (req, res) => {
