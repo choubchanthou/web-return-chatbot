d168a83255f0ffac2ff77c804c37922ce0e7966f
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-05 11:58:20 +0700

    test

--- routes/shipbacks.js
+++ routes/shipbacks.js
@@ -5,11 +5,12 @@ const query = require('../helpers/query');
 const srbAPI = require('../helpers/srb.service');
 
 router.post('/shipbacks/finish', async (req, res) => {
-    // const { order_number, label_url, voucher_url, page_id } = req.body || {};
+    const { psid } = req.body || {};
     // const { sender } = await query.session.find({ order_id: order_number });
     // const { access_token } = await query.user.find({ page_id });
     // await query.session.delete({ sender });
-    // await fbSend.sendReadReceipt(sender, access_token);
+    await fbSend.sendReadReceipt(sender, access_token);
+    await fbSend.sendMessage(psid, { text: 'success' });
     // await fbSend.sendDownloadLabelVoucher(sender,{ label_url, voucher_url }, access_token);
     console.log(req.body);
     res.json(req.body);
