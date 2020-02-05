46de4712adb63840503f839e767b08a0c13f2510
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-05 12:25:40 +0700

    add sender Id

--- routes/shipbacks.js
+++ routes/shipbacks.js
@@ -5,14 +5,13 @@ const query = require('../helpers/query');
 const srbAPI = require('../helpers/srb.service');
 
 router.post('/shipbacks/finish', async (req, res) => {
-    const { psid, tid } = req.body || {};
-    // const { sender } = await query.session.find({ order_id: order_number });
-    // const { access_token } = await query.user.find({ page_id });
+    const { psid } = req.body || {};
+    const { sender, page_id } = await query.session.fetchSession(psid);
+    const { access_token } = await query.user.find({ page_id });
     // await query.session.delete({ sender });
     // await fbSend.sendReadReceipt(sender, access_token);
-    // await fbSend.sendMessage(psid, { text: 'success' });
+    await fbSend.sendMessage(sender, { text: 'success' }, access_token);
     // await fbSend.sendDownloadLabelVoucher(sender,{ label_url, voucher_url }, access_token);
-    console.log(req.body);
     res.json(req.body);
 });
 
