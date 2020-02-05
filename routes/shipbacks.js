aa0e6decceead7b09778215c61494e691f5fac65
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-05 12:01:32 +0700

    test

--- routes/shipbacks.js
+++ routes/shipbacks.js
@@ -9,8 +9,8 @@ router.post('/shipbacks/finish', async (req, res) => {
     // const { sender } = await query.session.find({ order_id: order_number });
     // const { access_token } = await query.user.find({ page_id });
     // await query.session.delete({ sender });
-    await fbSend.sendReadReceipt(sender, access_token);
-    await fbSend.sendMessage(psid, { text: 'success' });
+    // await fbSend.sendReadReceipt(sender, access_token);
+    // await fbSend.sendMessage(psid, { text: 'success' });
     // await fbSend.sendDownloadLabelVoucher(sender,{ label_url, voucher_url }, access_token);
     console.log(req.body);
     res.json(req.body);
