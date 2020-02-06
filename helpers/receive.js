5714f9e20887405f47daf008a23420594a355192
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-06 12:41:10 +0700

    add feather

--- helpers/receive.js
+++ helpers/receive.js
@@ -9,19 +9,19 @@ const handleReceiveMessage = async (event, page_id) => {
         const senderId = event.sender.id;
         if (message.text) {
             await fbSend.sendReadReceipt(senderId, access_token);
-            const messageHello = ['hi','Hi', 'Hello', 'hello'];
-            if(messageHello.includes(message.text)) return await initMessage(senderId, access_token);
+            const messageHello = ['hi', 'Hi', 'Hello', 'hello'];
+            if (messageHello.includes(message.text)) return await initMessage(senderId, access_token);
             const { option, step } = query.session.fetchSession(senderId) || {};
-            if(message.text.toLowerCase() == 'new' || (option == 'new' && step == 0)) {
+            if (message.text.toLowerCase() == 'new' || (option == 'new' && step == 0)) {
                 await query.session.delete({ sender: senderId });
                 await query.session.insert({ sender: senderId, option: 'new', step: 0, page_id });
-                return fbSend.sendMessage(senderId, { text: 'Please enter your store name:'}, access_token);
+                return fbSend.sendMessage(senderId, { text: 'Please enter your store name:' }, access_token);
             }
             if (message.text.toLowerCase() == 'new2' || (option == 'new2' && step == 0)) {
                 await query.session.delete({ sender: senderId });
                 await query.session.insert({ sender: senderId, option: 'new2', step: 0, page_id });
                 return handlePostbackGetStarted(senderId, page_id, access_token);
-            } 
+            }
             return await handleMessage(senderId, page_id, message.text, access_token);
         }
     } catch (error) {
@@ -73,45 +73,53 @@ const handleMessage = async (senderId, page_id, message, access_token) => {
     if (!stores) return await fbSend.sendUnavailableStore(senderId, access_token);
     // return await fbSend.sendStoreList(senderId, stores, access_token);
     // await query.session.delete({ senderId });
-    
+
     // await query.session.insert({ senderId, store_name });
     var name = '';
-    for(let store of stores) {
-        if(store.name.toLowerCase() == message.toLowerCase()){
+    for (let store of stores) {
+        if (store.name.toLowerCase() == message.toLowerCase()) {
             name = store.fb_token;
             break;
         }
     }
-    if(name == '') return await fbSend.sendUnavailableStore(senderId, access_token);
+    if (name == '') return await fbSend.sendUnavailableStore(senderId, access_token);
     await query.session.update({ store_name: name, step: 1 }, { sender: senderId });
     return await fbSend.sendPleaseEnterOrder(senderId, access_token);
 };
 
 const handleReturnMessage = async (sender, store_name, message, access_token) => {
-    const { token } = await query.store.fetchStore(store_name);
-    if (!token) throw new TypeError("Unauthorize");
-    const { order_number } = await srbAPI.fetchOrder(message, token);
-    if (order_number == undefined) return await fbSend.sendTryEnterOrder(sender, access_token);
-    return await handleMessageOrder(sender, order_number, access_token, token);
+    try {
+        const { token } = await query.store.fetchStore(store_name);
+        if (!token) throw new TypeError("Unauthorize");
+        const { order_number } = await srbAPI.fetchOrder(message, token);
+        if (order_number == undefined) return await fbSend.sendTryEnterOrder(sender, access_token);
+        return await handleMessageOrder(sender, order_number, access_token, token);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 const handleMessageOrder = async (sender, order_number, access_token, token) => {
-    const shipbacks = await srbAPI.createShipback(order_number, token);
-    const shipback_id = shipbacks.id;
-    if(shipback_id == undefined) {
-        const messageRes = shipbacks.shipback.errors[0];
-        let arrayMessage = messageRes.split('(');
-        arrayMessage = arrayMessage[arrayMessage.length -1];
-        arrayMessage = arrayMessage.split(')');
-        const { public_url, charged, label_url, voucher_url } = await srbAPI.fetchShipback(arrayMessage[0], token);
-        if (charged) {
-            await fbSend.sendTracking(sender, public_url, access_token);
-            return await fbSend.sendDownloadLabelVoucher(sender, { label_url, voucher_url }, access_token);
+    try {
+        const shipbacks = await srbAPI.createShipback(order_number, token);
+        const shipback_id = shipbacks.id;
+        if (shipback_id == undefined) {
+            const messageRes = shipbacks.shipback.errors[0];
+            console.log(messageRes);
+            let arrayMessage = messageRes.split('(');
+            arrayMessage = arrayMessage[arrayMessage.length - 1];
+            arrayMessage = arrayMessage.split(')');
+            const { public_url, charged, label_url, voucher_url } = await srbAPI.fetchShipback(arrayMessage[0], token);
+            if (charged) {
+                await fbSend.sendTracking(sender, public_url, access_token);
+                return await fbSend.sendDownloadLabelVoucher(sender, { label_url, voucher_url }, access_token);
+            }
+            return fbSend.sendReturnShipback(sender, public_url, access_token);
         }
-        return fbSend.sendReturnShipback(sender, public_url, access_token);
+        return await fbSend.sendReturnShipback(sender, shipbacks.public_url, access_token);
+    } catch (error) {
+        console.log(error);
     }
-    return await fbSend.sendReturnShipback(sender, shipbacks.public_url, access_token);
-    
     // const { public_url } = await srbAPI.createShipback(order_number, token);
     // // await query.session.update({ shipback_id: id }, { sender });
     // return await fbSend.sendReturnShipback(sender, public_url, access_token);
