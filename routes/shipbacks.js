2d4a235c0011bb55723023032c69b6d522a2ebfb
Chanthou <chanthou@Chanthous-MacBook-Pro.local>
2020-02-17 13:55:50 +0700

    add tracking button

--- routes/shipbacks.js
+++ routes/shipbacks.js
@@ -6,6 +6,7 @@ const query = require('../helpers/query');
 router.post('/shipbacks/finish', async (req, res) => {
     const { psid, label_url, voucher_url, order_number } = req.body || {};
     const { sender, page_id } = await query.session.fetchSession(psid);
+    console.log('fetch session', { sender, page_id });
     const { access_token } = await query.user.fetchUser(page_id);
     await query.session.delete({ sender });
     await fbSend.sendReadReceipt(sender, access_token);
